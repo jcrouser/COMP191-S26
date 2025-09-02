aa8d006aea70c1a47a58fb74845ca5e7c44e4a5c
Jordan Crouser <crouser1@kenyon.edu>
2025-09-02 18:34:50 -0400

    Initial commit

---
+++ build_site.R
@@ -0,0 +1,8 @@
+# You may want to set your working directory. 
+# This helps avoid confusion if the working directory is 
+# not this website because of other projects you were 
+# working on at the time. 
+# setwd("/PATH/TO/REPO/GOES/HERE")
+
+#render site. 
+rmarkdown::render_site()
